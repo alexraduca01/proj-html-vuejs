89038c9e42cde4b14b3e4c05227067b3e96506d2
alexraduca01 <alex.raduca01@icloud.com>
2023-11-21 11:03:53 +0100

    added scaffolding

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,24 @@
 <template>
   <div>
-    
+    <HeaderComponent />
   </div>
 </template>
 
 <script>
+import HeaderComponent from './components/HeaderComponent.vue';
   export default {
-    
+    components: {
+      HeaderComponent,
+    },
+    data(){
+      return {
+
+      }
+    }
   }
 </script>
 
 <style lang="scss" scoped>
+@use './assets/style/partials/variables' as *;
 
 </style>
