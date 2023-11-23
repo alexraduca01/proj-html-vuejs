183f85dafb6b22a13e7f51ce6fac1383ef3d4b1e
alexraduca01 <alex.raduca01@icloud.com>
2023-11-23 11:32:25 +0100

    fixed AuthorComponent

--- src/components/AuthorComponent.vue
+++ src/components/AuthorComponent.vue
@@ -1,30 +1,39 @@
 <template>
     <div class="my-background my-5">
-        <div class="container p-5">
-            <div class="my-card py-5 gap-5 d-flex flex-column text-white align-items-center">
-                <div>
-                    <img :src="store.authors[this.activeIndex].thumb" alt="testimonial">
+        <swiper :loop="true"  :autoplay="{ delay: 3250, disableOnInteraction: false, }" :pagination="{ clickable: true, }" :modules="modules" class="mySwiper">
+            <swiper-slide v-for="item in store.authors">
+                <div class="container p-5">
+                    <div class="my-card py-5 gap-5 d-flex flex-column text-white align-items-center">
+                        <div>
+                            <img :src="item.thumb" alt="testimonial">
+                        </div>
+                        <p class="fs-5 text-center">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vel labore, fuga reprehenderit dignissimos quibusdam molestias dolores laudantium assumenda iusto dolor omnis doloribus quod nihil nemo laboriosam amet doloremque tenetur sunt incidunt possimus atque in? Temporibus blanditiis assumenda tenetur natus quae!</p>
+                        <p class="fs-5"><strong>{{ item.name }}</strong> Theme Fusion</p>
+                    </div>
                 </div>
-                <p class="fs-5 text-center">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vel labore, fuga reprehenderit dignissimos quibusdam molestias dolores laudantium assumenda iusto dolor omnis doloribus quod nihil nemo laboriosam amet doloremque tenetur sunt incidunt possimus atque in? Temporibus blanditiis assumenda tenetur natus quae!</p>
-                <p class="fs-5"><strong>{{ store.authors[this.activeIndex].name }}</strong> Theme Fusion</p>
-                <div class="d-flex justify-content-between gap-3">
-                    <div class="dot" :class="activeDot(1)" @click="changeAuthor(1), activeDot(1)"></div>
-                    <div class="dot" :class="activeDot(2)" @click="changeAuthor(2), activeDot(2)"></div>
-                </div>
-                
-            </div>
-        </div>
+            </swiper-slide>
+        </swiper>
     </div>
 </template>
 
 <script>
+import { Swiper, SwiperSlide } from 'swiper/vue';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import { Pagination } from 'swiper/modules';
+import { Autoplay } from 'swiper/modules';
 import { store } from '../data/store.js';
     export default {
+        components: {
+            Swiper,
+            SwiperSlide,
+        },
         name: 'AuthorComponent',
         data(){
             return {
                 store,
                 activeIndex: 0,
+                modules: [Pagination, Autoplay],
             }
         },
         methods: {
@@ -77,4 +86,5 @@ img {
     background-repeat: no-repeat;
     background-attachment: fixed;
 }
+
 </style>
