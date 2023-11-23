97f85ee9b7d0f9bc2e29a7960b7b21c913c31e24
alexraduca01 <alex.raduca01@icloud.com>
2023-11-23 10:55:25 +0100

    fixed BrandsComponent

--- src/components/BrandsComponent.vue
+++ src/components/BrandsComponent.vue
@@ -1,23 +1,37 @@
 <template>
     <div class="my-bg p-5">
         <div class="container">
-            <p class="text-center fs-5 text-uppercase my-4">brand logos</p>
+            <p class="text-center fs-5 text-uppercase my-5">brand logos</p>
             <div class="row overflow-hidden flex-nowrap">
-                <div class="img-container" v-for="item in store.brandLogos">
-                    <img :src="item" alt="">
-                </div>
+                <swiper :loop="true" :slidesPerView="5" :spaceBetween="30" :pagination="{ clickable: true, }" :modules="modules" class="mySwiper" >
+                    <swiper-slide v-for="item in store.brandLogos">
+                        <div class="img-container" >
+                            <img :src="item" alt="">
+                        </div>
+                    </swiper-slide>
+                </swiper>
             </div>
         </div>
     </div>
+    
 </template>
 
 <script>
+import { Swiper, SwiperSlide } from 'swiper/vue';
+import 'swiper/css';
+import 'swiper/css/pagination';
 import { store } from '../data/store.js';
+import { Autoplay } from 'swiper/modules';
     export default {
+        components: {
+            Swiper,
+            SwiperSlide,
+        },
         name: 'BrandsComponent',
         data(){
             return {
                 store,
+                modules: [Autoplay]
             }
         }
     }
